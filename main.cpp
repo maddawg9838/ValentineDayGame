0e84cd995a09156dab5905ea060561082d02c3f0
Madison Humphries <125151938+maddawg9838@users.noreply.github.com>
2024-09-25 19:31:14 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,170 +1,87 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 #include <string>
 
 using namespace std;
 
-// Completed: 6-12-23! 
-// Author: Maddawg9838
+const int TOTAL_LETTERS = 8;
+const string SECRET_PHRASE = "ILoveYou";
 
-void play ();
-void game ();
-void display ();
+void play();
+void game();
+void displayHeart();
 
-//Function Main
-int main()
-{
-play ();
-game ();
-display();
-return(0);
+int main() {
+    play();
+    return 0;
 }
 
-
-//Function Play is a prelude message before the game
-void play ()
-{
-char decision;
-
-cout << "Let’s Play a Game!" << endl;
-cout << "Do you wanna play? (Y for yes or N for no)" << endl;
-cin >> decision;
+void play () {
+    char decision;
+    cout << "Let’s Play a Game!" << endl;   
+    cout << "Do you wanna play? (Y for yes or N for no)" << endl;
+    cin >> decision;
 
 
-    while(decision != 'Y' && decision != 'y' && decision != 'N' && decision != 'n' )
+    while(decision != 'Y' && decision != 'y')
     {
-        cout << "Can you even spell? Try again" << endl;
-        cin >> decision;
-        
-        if(decision == 'Y' || decision == 'y')
-        {
-            cout << "Yay!" << endl;
-        }
-        else if (decision == 'N' || decision == 'n')
-        {
-            cout << "Too bad. We are gonna play anyway" << endl;
-        }
+        cout << "Too bad. We are going to play anyways!" << endl;
+        break;
     }
 
-    cout << "We are gonna play hangman" << endl;
-    cout << "Try and guess the statement" << endl << endl;
+    game();
 }
 
-//Function Game matches the letters and gives a congradulations message when guessed correctly
-void game ()
-{
-    char letter, letter1 = '_', letter2 = '_', letter3 = '_', letter4 = '_', letter5 = '_', letter6 = '_', letter7 = '_', letter8 = '_';
-    string statement;
-    int letters = 0;
-    bool win = false;
+void game() {
+    vector<char> guessed(TOTAL_LETTERS, '_');
+    int correctGuesses = 0;
     
-    cout << letter1 << " " << letter2 << letter3 << letter4 << letter5 << " " << letter6 << letter7 << letter8 << "!" << endl;
-    cout << "Well…give it a go and guess a letter (use all capital letters or I will judge you for not correctly reading this message" << endl;
-    cin >> letter;
+    cout << "Guess the statement!" << endl;
     
-    // Since this is a preset word, the program uses a switch with the matching 
-    while (win == false)
-    {
-        switch (letter)
-        {
-            case 'I': {
-                letter1 = 'I';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
+    while (correctGuesses < TOTAL_LETTERS) {
+        char letter;
+        for (char c : guessed) {
+            cout << c << ' ';
+        }
+        cout << "Guess a letter: ";
+        cin >> letter;
+        letter = toupper(letter);
+        
+        // Check for the letter in the secret phrase
+        bool found = false;
+        for (size_t i = 0; i < SECRET_PHRASE.length(); i++) {
+            if (toupper(SECRET_PHRASE[i]) == letter && guessed[i] == '_') {
+                guessed[i] = letter;
+                correctGuesses++;
+                found = true;
+                cout << "Good job!" << endl;
             }
-            case 'L':
-            {
-                letter2 = 'L';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
-            }
-              
-            case 'O':
-            {
-                letter3 = 'O';
-                letter7 = 'O';
-                cout << "Double Goodjob!" << endl;
-                letters+= 2;
-                break;
-            }
-             
-            case 'V':
-            {
-                letter4 = 'V';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
-            }
-           
-            case 'E':
-            {
-                letter5 = 'E';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
-            }
-          
-            case 'Y':
-            {
-                letter6 = 'Y';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
-            }
-           
-            case 'U':
-            {
-                letter8 = 'U';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
-            }
-              
-            default:
-            {
-                cout << "Guess the right letter or read the instructions correctly" << endl;
-                cout << "Go ahead. Guess again. This time make it a good one" << endl;
-                cin >> letter;
-            }
-                
         }
         
-        if(letters == 8)
-        {
-            win = true;
-            cout << "You Guessed It!" << endl;
-            cout << letter1 << " " << letter2 << letter3 << letter4 << letter5 << " " << letter6 << letter7 << letter8 << "!" << endl;
-            
-            cout << "Wait, really? Thanks!" << endl << endl;
-            cout << "Just Kidding :)" << endl;
+        if (!found) {
+            cout << "Try again!" << endl;
         }
-        else
-        {
-            cout << letter1 << " " << letter2 << letter3 << letter4 << letter5 << " " << letter6 << letter7 << letter8 << "!" << endl;
-            cout << "Keep Guessing. You Got This!" << endl;
-            cin >> letter;
+
+        if (correctGuesses == TOTAL_LETTERS) {
+            cout << "You guessed it! The phrase is: " << SECRET_PHRASE << endl;
+            break;
         }
     }
+    displayHeart();
 }
 
-    //Function Display displays a heart out of astericks
-    void display()
+//Function Display displays a heart out of astericks
+void displayHeart()
 {
-        int n,row,col;
-        n = 6;
-        cout << " I Love You <3 " << endl;
-        for(row=0;row<n;row++)
-        {
-          for(col=0;col<n+1;col++)
-             if ((row==0 && col%3!=0) || (row==1 && col%3==0) || (row-col==2) || (row+col==8))
-                  cout<<("*");
-                else
-                  cout<<(" ");
-
-            cout<<("\n");
+    cout << " I Love You <3 " << endl;
+    for (int row = 0; row < 6; row++) {
+        for(int col = 0; col < 6; ++col) {
+            if ((row == 0 && col % 3 != 0) || (row == 1 && col % 3 == 0) || (row - col == 2) || (row + col == 8)) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
         }
+        cout << endl;
     }
-
-
+}
